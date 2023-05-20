c97e6a800b795fde89a1aeeea7cc871f203b87d3
Denis Kolesnikov <116916114+Lenis45@users.noreply.github.com>
2023-05-20 19:12:44 +0300

    update courseRouter

--- src/server/routes/courseRouter.js
+++ src/server/routes/courseRouter.js
@@ -3,7 +3,7 @@ const router = new Router()
 const courseController = require('../controllers/courseController')
 const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', checkRole('ADMIN'), deviceController.create)
+router.post('/', checkRole('ADMIN'), courseController.create)
 router.get('/', courseController.getAll)
 router.get('/:id', courseController.getOne)
 
